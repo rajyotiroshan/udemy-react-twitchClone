4fe4abc57afd7d6c042b61a1113cf3e96de0d5d3
rajan <rajangreedge@gmail.com>
2019-04-14 20:43:46 +0530

    used redux, react-redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,27 @@
-import React, { Component } from 'react';
-import {BrowserRouter, Route, Link } from 'react-router-dom';
-
-const PageOne = ()=>{
-  return (
-    <div>
-      <Link to="/pageTwo">Navigate to page 2</Link>
-    </div>)
-};
-
-const PageTwo = ()=> {
-  return (
-    <div>
-      PageTwo
-      <button>Click me</button>
-      <Link to="/">Navigate to page 1</Link>
-      </div>)
-};
-
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from './streams/StreamShow';
+import StreamList from './streams/StreamList';
+import Header from './Header'
 
 const App = ()=>{
   return (
-    <div>
+    <div className='ui container'>
       <BrowserRouter>
+        <Header />  
         <div>
-          <Route path="/" exact component={PageOne}/>
-          <Route path="/pageTwo" component={PageTwo}/>        
+          <Route path='/' exact component={StreamList} />
+          <Route path='/streams/new' exact component={StreamCreate} />
+          <Route path='/streams/edit' exact component={StreamEdit} />
+          <Route path='/streams/show' exact component={StreamShow} />
+          <Route path='/streams/delete' exact component={StreamDelete} />
         </div>
       </BrowserRouter>
     </div>)
 }
 export default App;
+
+//530995356834-4ruj6khbmt57ektbcs2jseoecordoo74.apps.googleusercontent.com
