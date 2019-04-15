7aae46839c397bec52362d8f1908a046080661db
rajan <rajangreedge@gmail.com>
2019-04-15 18:48:42 +0530

    created title and description input field

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,6 +1,35 @@
 import React from 'react';
+import { Field, reduxForm} from 'redux-form';
+ class StreamCreate extends React.Component{
 
- const StreamCreate = ()=>{
-    return <div>Stram Create</div>
+    renderInput({input, label}) {
+        //console.log(formProps)
+        //m-1
+/*         return <input onChange={formProps.input.onChange}
+                      value = {formProps.input.value} />; */
+        //m-2
+        return (
+                <div className="field">
+                    <label>{label}</label> 
+                    <input {...input}/> 
+                </div>
+                )
+    }
+
+     render(){
+         //reduxForm passess an object with different properties as a props to this component.
+         //console.log(this.props)
+         return(
+             <form className="ui form">
+                 <Field name="title" component={this.renderInput} label="Enter Title"/>
+                 <Field name="description" component={this.renderInput} label="Enter Description"/>
+             </form>
+         );
+     }
+  
 }
-export default StreamCreate;
+
+//hook up StreamCreate to reduxForm.
+export default reduxForm({
+    form: 'streamCreate', //name of the form anything.
+})(StreamCreate);
