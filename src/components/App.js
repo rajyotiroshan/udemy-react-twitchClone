cde6022684564635580ab1d65ab45fef8eaf13a2
rajan <rajangreedge@gmail.com>
2019-04-17 19:57:20 +0530

    used explicit history object and plain Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {Router, Route} from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamShow from './streams/StreamShow';
 import StreamList from './streams/StreamList';
-import Header from './Header'
+import Header from './Header';
+import browserHistory from '../history';
 
 const App = ()=>{
   return (
     <div className='ui container'>
-      <BrowserRouter>
+      <Router history={browserHistory}>
         <Header />  
         <div>
           <Route path='/' exact component={StreamList} />
@@ -19,7 +20,7 @@ const App = ()=>{
           <Route path='/streams/show' exact component={StreamShow} />
           <Route path='/streams/delete' exact component={StreamDelete} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>)
 }
 export default App;
