9a7ca8b413af6ac8cd68c366e808eb35d0c83d6a
rajan <rajangreedge@gmail.com>
2019-04-17 19:58:44 +0530

    included streamReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import {combineReducers} from 'redux';
 import {reducer as formReducer} from 'redux-form';
-import authReducer from './authReducer'
+import authReducer from './authReducer';
+import streamReducer from './streamReducer';
+
 
 export default combineReducers({
     auth: authReducer,
+    streams: streamReducer,
     form: formReducer //use form only.
 });
