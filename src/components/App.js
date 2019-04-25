5952a6541314d269bf7f6cd1344d197b68017314
rajan <rajangreedge@gmail.com>
2019-04-25 11:17:29 +0530

    use Switch to wrap route

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,31 @@
-import React from 'react';
-import {Router, Route} from 'react-router-dom';
-import StreamCreate from './streams/StreamCreate';
-import StreamEdit from './streams/StreamEdit';
-import StreamDelete from './streams/StreamDelete';
-import StreamShow from './streams/StreamShow';
-import StreamList from './streams/StreamList';
-import Header from './Header';
-import browserHistory from '../history';
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamShow from "./streams/StreamShow";
+import StreamList from "./streams/StreamList";
+import Header from "./Header";
+import browserHistory from "../history";
 
-const App = ()=>{
+const App = () => {
   return (
-    <div className='ui container'>
+    <div className="ui container">
       <Router history={browserHistory}>
-        <Header />  
         <div>
-          <Route path='/' exact component={StreamList} />
-          <Route path='/streams/new' exact component={StreamCreate} />
-          <Route path='/streams/edit/:id' exact component={StreamEdit} />
-          <Route path='/streams/:id' exact component={StreamShow} />
-          <Route path='/streams/delete/:id' exact component={StreamDelete} />
+          <Header />
+          <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit/:id" exact component={StreamEdit} />
+            <Route path="/streams/:id" exact component={StreamShow} />
+            <Route path="/streams/delete/:id" exact component={StreamDelete} />
+          </Switch>
         </div>
       </Router>
-    </div>)
-}
+    </div>
+  );
+};
 export default App;
 
 //530995356834-4ruj6khbmt57ektbcs2jseoecordoo74.apps.googleusercontent.com
